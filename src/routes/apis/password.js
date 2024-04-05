7d43146ef8735d5aff13d5326acc6ac283bb0e59
pphatdev <leatsophat02@gmail.com>
2024-04-05 16:43:09 +0700

    Created Authenticate

--- src/routes/apis/password.js
+++ src/routes/apis/password.js
@@ -2,13 +2,11 @@ import { Router } from 'express'
 import { updatePassword } from '../../controllers/password.js'
 export const ROUTE = Router()
 
-
 ROUTE.put("/",  async (req, res) =>
 {
     const request  = req.body
     const data     = await updatePassword(request)
     res.send(data)
 })
-
 
 export default ROUTE
