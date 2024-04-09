8e5ac33a0cb195ec3c3149423c9f2b289bc171a9
pphatdev <leatsophat02@gmail.com>
2024-04-09 08:24:01 +0700

    Fix code scanning alert - Database query built from user-controlled sources

--- src/models/password.js
+++ src/models/password.js
@@ -12,27 +12,31 @@ const PAGE      = new Pagination()
 export const getData = async ( request ) =>
 {
     const { page, limit, search, sort } = request
-    const count         = await client.query(`SELECT count(id) from public.users`)
-    const total         = count.rows[0].count || 0
-    const pagination    = PAGE.list({
-        page: page,
-        limit: limit,
-        search: {
-            column: [ 'name' ],
-            value: search,
-            condition: "or",
-            withWere: true
-        },
-        sort: {
-            column: [ "name" ],
-            value: sort
-        },
-    })
-
-    const query     = `SELECT id, name, password from public.users ${pagination}`
+    const count = await client.query(`SELECT count(id) from public.users`)
+    const total = count.rows[0].count || 0
+    const query = String(
+        `SELECT id, name, password from public.users ${
+            PAGE.list(
+                {
+                    page: page,
+                    limit: limit,
+                    search: {
+                        column: [ 'name' ],
+                        value: search,
+                        condition: "or",
+                        withWere: true
+                    },
+                    sort: {
+                        column: [ "name" ],
+                        value: sort
+                    },
+                }
+            )
+        }`
+    )
 
     return await client.query(query).then(
-        async result => {
+        result => {
             const data = {
                 data: result.rows,
                 count: total,
