38c2b26a634cb2e181fc43801ce36e0556137961
pphatdev <leatsophat02@gmail.com>
2025-06-22 18:30:48 +0700

    🐛 refactor: streamline response handling in authenticate middleware for consistency

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,29 +3,28 @@ import { Response } from "../helpers/response-data.js";
 import { APP_SECRET_KEY } from "../db/configs/index.js";
 
 const { verify }    = jwt;
-const response      = new Response()
 
 export const authenticateToken = (req, res, next) =>
 {
     /**
      * Get Auth bearer token
     */
-    const authHeader    = req.headers['authorization'];
-    const token         = authHeader && authHeader.split(' ')[1];
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
 
     /**
      * ករណីគ្មាន Token
     */
     if (!token)
-        return res.send(response.unAuth({ message : "Unauthorized." }));
+        return res.send(Response.unAuth({ message : "Unauthorized." }));
 
     /**
      * Verify Token
     */
     verify( token, APP_SECRET_KEY, (err, decoded) => {
         if (err)
-            return res.send(response.invalidToken({ message : "Token is invalid." }));
+            return res.send(Response.invalidToken({ message : "Token is invalid." }));
 
         req.user = decoded;
         next();
