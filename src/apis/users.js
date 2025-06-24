26b8f451010d222cdfef7e8fc1f5e59c314881db
pphatdev <pphat.kits@gmail.com>
2025-06-24 20:05:01 +0700

    🐛 fix: refactor user routes to remove Controller and directly use async functions for improved clarity

--- src/apis/users.js
+++ src/apis/users.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { create, update, get , getOnce } from '../controllers/users.js'
 import { authenticateToken } from '../middlewares/authenticate.js'
 import { Validation } from '../helpers/validator.js'
-import { Controller } from '../helpers/response/controller.js'
+import { getData } from '../models/users.js'
 
 export const ROUTE = Router()
 
@@ -12,12 +12,18 @@ ROUTE.use((req, res, next) => authenticateToken(req, res, next))
 
 ROUTE.get("/",
     Validation.base.list,
-    async (req, res) => Controller.get(req, res, get)
+    async (req, res) => {
+        const response = await getData(req.params)
+        res.send(response)
+    }
 )
 
 ROUTE.get("/:id",
     Validation.base.id,
-    async (req, res) => Controller.getOnce(req, res, getOnce)
+    async (req, res) => {
+        const response = await getOnce(req.params)
+        res.send(response)
+    }
 )
 
 ROUTE.put("/", async (req, res) => {
