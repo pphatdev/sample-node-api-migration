fceb18847ccd6f73e1505a3cbdbc2e86f0524a74
pphatdev <pphat.kits@gmail.com>
2025-06-24 20:04:27 +0700

    🐛 fix: format code for consistency in auth.js

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -4,7 +4,7 @@ import { login } from '../controllers/auth.js'
 export const ROUTE = Router()
 
 ROUTE.post( "/", async (req, res) => {
-    const response     = await login(req.body)
+    const response = await login(req.body)
     res.send(response)
 })
 
