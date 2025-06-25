c8d741b05ce0a0bc0b2bcf236866b78a028ea310
pphatdev <pphat.kits@gmail.com>
2025-06-25 11:31:21 +0700

    🐛 fix: enhance paramsToNameFile to handle nested objects in parameter conversion

--- src/helpers/utils/convertion/string.js
+++ src/helpers/utils/convertion/string.js
@@ -3,7 +3,7 @@ export const paramsToNameFile = (params) => {
         return '';
     }
     return Object.entries(params)
-        .map(([key, value]) => `${key}-${String(value).replace(/![a-zA-Z0-9]$/g, "")}`)
+        .map(([key, value]) => (typeof value === 'object' && value !== null) ? paramsToNameFile(value) : `${key}-${String(value).replace(/![a-zA-Z0-9]$/g, "")}`)
         .join('_')
         .replace(/[^a-zA-Z0-9_-]/g, '_') // Replace any non-alphanumeric characters with underscores
         .toLowerCase(); // Convert to lowercase for consistency
